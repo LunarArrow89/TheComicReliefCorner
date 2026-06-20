5fda2e0d83f92040d0ef5b52d54992053aee52db
LunarArro89 <fmmurray14@gmail.com>
2026-06-20 11:58:12 -0400

    Update load.js

--- load.js
+++ load.js
@@ -19,7 +19,6 @@ loadHTML("sidebar-container", "sidebar.html");
 /* Load header THEN start clock */
 loadHTML("header-container", "header.html", startClock);
 
-
 function startClock() {
     function updateClock() {
         const timeBox = document.getElementById("timeBox");
@@ -32,21 +31,7 @@ function startClock() {
         timeBox.innerText = now.toLocaleTimeString();
         dateBox.innerText = now.toLocaleDateString();
     }
-/* =========================
-   CUSTOM CURSOR
-========================= */
 
-body {
-    cursor: url("cursor-default.png"), auto;
-}
-
-a,
-button,
-.nav-btn,
-input,
-textarea {
-    cursor: url("cursor-hover.png"), pointer;
-}
     updateClock();
     setInterval(updateClock, 1000);
 }
